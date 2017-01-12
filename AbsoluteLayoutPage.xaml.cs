24d7041f6e748b641ca2bc28b20266cfea64806d
jordanpurinton <jordanomac@gmail.com>
2017-01-12 10:12:02 -0700

    implement AbsoluteLayout in code behind

--- AbsoluteLayoutPage.xaml.cs
+++ AbsoluteLayoutPage.xaml.cs
@@ -10,6 +10,16 @@ namespace HelloWorld
 		public AbsoluteLayoutPage()
 		{
 			InitializeComponent();
+
+			//var layout = new AbsoluteLayout();
+			//Content = layout;
+
+			//var box = new BoxView { Color = Color.Aqua };
+			//layout.Children.Add(box
+			//                    , new Rectangle(0, 0, 1, 1)
+			//                    , AbsoluteLayoutFlags.All);
+			//AbsoluteLayout.SetLayoutBounds(box, new Rectangle(0, 0, 1, 1));
+			//AbsoluteLayout.SetLayoutFlags(box, AbsoluteLayoutFlags.HeightProportional);
 		}
 	}
 }
