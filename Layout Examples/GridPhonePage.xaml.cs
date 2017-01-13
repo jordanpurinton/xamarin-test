f159c2311e506236a44ebc49f4241b8d813d88f5
jordanpurinton <jordanomac@gmail.com>
2017-01-13 09:59:20 -0700

    modify file structure

---
+++ Layout Examples/GridPhonePage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace HelloWorld
+{
+	public partial class GridPhonePage : ContentPage
+	{
+		public GridPhonePage()
+		{
+			InitializeComponent();
+			dialedLabel.Text = "Enter number";
+		}
+
+		void Handle_Clicked(object sender, System.EventArgs e)
+		{
+			//
+		}
+	}
+}
