c87ecfe85bd4a3845b372f0a5892e2f547230ab4
jordanpurinton <jordanomac@gmail.com>
2017-01-13 10:58:00 -0700

    alert functionality w/ action sheet and popups

---
+++ Alert Examples/ToolbarItemExample.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace HelloWorld
+{
+	public partial class ToolbarItemExample : ContentPage
+	{
+		public ToolbarItemExample()
+		{
+			InitializeComponent();
+		}
+
+		void Handle_Activated(object sender, System.EventArgs e)
+		{
+			DisplayAlert("ACTIVATE", "You activated me!", "Dismiss");
+		}
+	}
+}
