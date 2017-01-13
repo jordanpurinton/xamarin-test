9f7add53009be6a12bcb5831b9ff1b460e2994be
jordanpurinton <jordanomac@gmail.com>
2017-01-13 14:23:46 -0700

    date and time selection picker

---
+++ Forms & Settings Examples/DatePickerEx.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace HelloWorld
+{
+	public partial class DatePickerEx : ContentPage
+	{
+		public DatePickerEx()
+		{
+			InitializeComponent();
+		}
+
+		void Handle_DateSelected(object sender, Xamarin.Forms.DateChangedEventArgs e)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
